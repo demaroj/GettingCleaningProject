b190aab4762d60360b00b13e5fb810e6d3fb5a16
Joe DeMaro <demaroj@gmail.com>
2018-08-17 11:33:05 -0500

    found unzip and read.table for files in dataset

--- GettingCleaningData/GettingCleaningScript.R
+++ GettingCleaningData/GettingCleaningScript.R
@@ -1,12 +1,14 @@
 #install.packages("plyr")
 library(plyr)
 
-temp <- tempfile()
 zipfile <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(zipfile, tf <- tempfile(fileext = ".zip"))
+unzip(tf, exdir = td <- file.path(tempdir(), "myzip"))
+(list.files(td, full.names = TRUE, recursive = TRUE))
+#list(tempdir())
 
-download.file(zipfile,temp)
 
-unzip(zipfile, list = TRUE)
-#y_testdata <- read.table(zipfile, "y_test.txt") 
-x_testdata <- read.table("C:\\RPrograms\\GettingCleaningProject\\GettingCleaningData\\UCI HAR Dataset\\test\\X_test.txt")
+x_testdata <- read.table(file.path(tempdir(), "/myzip/UCI HAR Dataset/test/X_test.txt"))
+
+
 head(x_testdata)
